2e5edbb2d1a1a61f8e8271d59eb42911845de92a
cardugarte <cardugarte@users.noreply.github.com>
2022-09-14 18:33:57 +0000

    deploy: 35e2ac5d8296b0034ac4ef24345890895fc7eede

--- _dist_/index.js
+++ _dist_/index.js
@@ -2,22 +2,35 @@
  * This file is just a silly example to show everything working in the browser.
  * When you're ready to start on your site, clear the file. Happy hacking!
  **/
-const url = "https://platzi-avo.vercel.app/api/avo";
+const urlBase = 'https://platzi-avo.vercel.app';
+const urlApi = '/api/avo';
 const allItems = [];
+const appNode = document.querySelector('#container');
 
 //petición con promesas
-
-window.fetch( url )
+window.fetch( `${ urlBase }${ urlApi }` )
  .then( ( avocados ) => avocados.json()
  .then( ( res ) => {
   res.data.forEach( element => {
+    //Image
   const image = document.createElement('img');
+  image.src = `${ urlBase }${ element.image }`;
+  image.className = 'img';
+  //Title
   const title = document.createElement('h2');
+  title.textContent = element.name;
+  // title.style.fontSize = '3rem';
+  title.className = 'title';
+  //Price
   const price = document.createElement('span');
+  price.textContent = element.price;
+  price.className = 'price'
+  //Container
   const container = document.createElement('div')
   container.append( image, title, price );
-  allItems.push(container);
+  allItems.push( container );
+  container.className = 'container__avocado';
  });
 
- document.body.append(...allItems);
+ appNode.append( ...allItems );
 }))
